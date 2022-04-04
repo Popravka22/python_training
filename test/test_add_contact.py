9a5e66869fc6741919da89b695c189fd05709bf1
neka <n-popravko@yandex.ru>
2022-04-04 12:08:52 +0300

    Реализован тест Создание пустого контакта

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -3,5 +3,11 @@ from model.contact import Contact
 def test_add_contact(app):
     app.session.login(username="admin", password="secret")
     #create contact
-    app.contact.create_contact(Contact(firstname="Vasya", lastname="Pupkin", company="Opa", mobile="+79112223344", email="vasya@test.test"))
+    app.contact.create_contact(Contact(firstname="Vasya", lastname="Pupkin", company="Paparam", mobile="+79112223344", email="vasya@test.test"))
+    app.session.logout()
+
+def test_add_empty_contact(app):
+    app.session.login(username="admin", password="secret")
+    #create contact
+    app.contact.create_contact(Contact(firstname="", lastname="", company="", mobile="", email=""))
     app.session.logout()
