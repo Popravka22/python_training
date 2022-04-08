80961633229396df5b6d3aa699921d7f73579098
neka <n-popravko@yandex.ru>
2022-04-08 13:41:24 +0300

    Добавлена проверка на наличие хотя бы одной группы (и создание тестовой группы при необходимости) в тесты на удаление и редактирование группы

--- test/test_modify_gorup.py
+++ test/test_modify_gorup.py
@@ -1,10 +1,16 @@
 from model.group import Group
 
 def test_modify_first_group_name(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="test"))
     app.group.modify_first(Group(name="New_group"))
 
 def test_modify_first_group_header(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="test"))
     app.group.modify_first(Group(header="New_header"))
 
 def test_modify_first_group_footer(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="test"))
     app.group.modify_first(Group(footer="New_footer"))
